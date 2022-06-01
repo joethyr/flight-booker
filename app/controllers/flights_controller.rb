24d8a20eba4ca070184521690f093fc3733cae3d
joethyr <joethayr@gmail.com>
2022-06-01 13:10:39 -0600

    add bootstrap

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,4 +2,10 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_flight
+    @flight = Flight.find(params[:id])
+  end
 end
