192aac1d9cb0d5772f77bb3ce56b313120484830
joethyr <joethayr@gmail.com>
2022-05-31 22:10:37 -0600

    populate seed file with data for Airport codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.create(code: "ATL")
+Airport.create(code: "DFW")
+Airport.create(code: "DEN")
+Airport.create(code: "ORD")
