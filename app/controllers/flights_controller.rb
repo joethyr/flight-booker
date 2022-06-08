06fde0a830aca6e97e2f0b6b88a5d04a5baab2ad
joethyr <joethayr@gmail.com>
2022-06-07 19:40:01 -0600

    create Booking model

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,11 @@
 class FlightsController < ApplicationController
 
   def index
-    @airport_options = Airport.all.map{|i| [i.code, i.id] }
-    @flights = Flight.all
-    @available_flights = Flight.where(depart_airport_id: params[:depart_airport_id], arrival_airport_id: params[:arrival_airport_id], depart_time: params[:depart_time])
+    # @airport_options = Airport.all.map { |i| [i.code, i.id] }
+
+    @available_flights = Flight.where(flight_params) unless flight_params.empty?
+
+    # @available_flights = Flight.where(depart_airport_id: params[:depart_airport_id], arrival_airport_id: params[:arrival_airport_id], depart_time: params[:depart_time])
   end
 
   private
@@ -14,6 +16,6 @@ class FlightsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def flight_params
-    params.require(:flight).permit(:depart_airport_id, :arrival_airport_id, :depart_time, :duration)
+    params.permit(:depart_airport_id, :arrival_airport_id, :depart_time)
   end
 end
